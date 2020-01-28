d961982df7c570cc20043ec6aa9686eaeb5a8169
Nicolas Rodríguez Hernández-Carrillo <Nrodriguez@axesor.es>
2020-01-28 17:44:09 +0100

    test reducer solicitudes mal

--- src/reducers/solicitudes.test.ts
+++ src/reducers/solicitudes.test.ts
@@ -1,5 +1,34 @@
+import reducer from './solicitudes'
+import * as types from '../actions/solicitudes'
+import ComponentController from "../classes/ComponentController";
+
 describe(" Solicitudes Reducer", () => {
-    it("SAMPLE", () => {
-        expect(1).toBe(1);
+    it("Tiene que retornar el estado inicial", () => {
+        expect(reducer(undefined, {type: ""})).toEqual([
+            {
+                cargadas: [],
+                editable: false,
+            }
+        ]);
+    })
+
+    it("Tiene que hacer la accion ACTION_SOLICITUDES", () => {
+        expect(
+            reducer(undefined, {
+                type: types.ACTION_SOLICITUDES
+            })
+        ).toEqual([
+            {
+                editable: false,
+                cargadas: [
+                    {
+                        id: ComponentController.obtenerNuevoId(),
+                        bloqueada: false,
+                        colaboradorId: 1,
+                        productoId: 1,
+                    }
+                ]
+            }
+        ])
     })
 });
