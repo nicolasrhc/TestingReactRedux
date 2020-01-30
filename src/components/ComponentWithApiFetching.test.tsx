81bed9eb65f04c176dd628c2444641506130b829
Nicolas Rodríguez Hernández-Carrillo <Nrodriguez@axesor.es>
2020-01-30 08:15:05 +0100

    ComponentWithApiFetching Test

--- src/components/ComponentWithApiFetching.test.tsx
+++ src/components/ComponentWithApiFetching.test.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import ComponentWithApiFetching from './ComponentWithApiFetching'
+import { shallow, ShallowWrapper } from 'enzyme'
+import ComponentController from "../classes/ComponentController";
+import Dato from "../interfaces/Dato";
 
 describe("ComponentWithApiFetching Test", () => {
+    let wrapper: ShallowWrapper;
+
+    beforeEach(() => {
+        
+    })
+
     it("Debe renderizarse correctamente", () => {
-        expect(1).toBe(1);
+        wrapper = shallow(<ComponentWithApiFetching/>);
+        expect(wrapper).toMatchSnapshot()
     });
+
+    it("Debe modificar el estado inicial", () => {
+        const obtenerDatosServidorMockedResponse: Dato[] = [{id: 1, nombre: "A", apellido: "B", fecha: new Date()}];
+
+        const spyObtenerDatosServidor = jest.spyOn(ComponentController.prototype, "obtenerDatosServidor").mockResolvedValue(obtenerDatosServidorMockedResponse);
+
+        wrapper = shallow(<ComponentWithApiFetching/>);
+        
+        expect(spyObtenerDatosServidor).toBeCalledTimes(1)
+    })
 });
