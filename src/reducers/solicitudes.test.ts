bcf976442783557fe5e5b4738fbc3869976ec615
Nicolas Rodríguez Hernández-Carrillo <Nrodriguez@axesor.es>
2020-01-30 08:15:44 +0100

    Solicitudes reducer test

--- src/reducers/solicitudes.test.ts
+++ src/reducers/solicitudes.test.ts
@@ -4,31 +4,34 @@ import ComponentController from "../classes/ComponentController";
 
 describe(" Solicitudes Reducer", () => {
     it("Tiene que retornar el estado inicial", () => {
-        expect(reducer(undefined, {type: ""})).toEqual([
-            {
-                cargadas: [],
-                editable: false,
-            }
-        ]);
+        expect(reducer(undefined, {type: ""})).toEqual({
+            cargadas: [],
+            editable: false,
+        });
     })
 
     it("Tiene que hacer la accion ACTION_SOLICITUDES", () => {
+        const idMockValue = 2;
+        const obtenerNuevoIdSpy = jest.spyOn(ComponentController, "obtenerNuevoId").mockReturnValue(idMockValue)
+
         expect(
             reducer(undefined, {
                 type: types.ACTION_SOLICITUDES
             })
-        ).toEqual([
+        ).toEqual(
             {
                 editable: false,
                 cargadas: [
                     {
-                        id: ComponentController.obtenerNuevoId(),
+                        id: idMockValue,
                         bloqueada: false,
                         colaboradorId: 1,
                         productoId: 1,
                     }
                 ]
             }
-        ])
+        )
+
+        expect(obtenerNuevoIdSpy).toBeCalledTimes(1);
     })
 });
